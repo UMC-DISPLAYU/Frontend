29636f989ab71eed86236c57396140f63b218ced
Leeseungchul <chulee503@naver.com>
2026-07-03 02:19:04 +0900

    feat: 홈 페이지 라우팅 설정 (#13)
    * feat: 홈페이지 라우팅 설정 완료
    
    * feat: 홈페이지 라우팅 설정 완료

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { Homepage } from './pages/Homepage';
+import { NotFound } from './pages/NotFound';
 import { App } from './App';
-import { NotFound } from './NotFound';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      { index: true, element: <Homepage /> },
+      // 새 페이지 여기에 추가
+      // { path: 'about', element: <About /> },
+    ],
   },
   {
     path: '*',
