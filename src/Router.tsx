2520362960740c03c9e51d9f20e94960a6529a6f
anjaein <103022804+anjaein@users.noreply.github.com>
2026-07-02 16:16:55 +0900

    feat: 라우팅 설정 (#7)
    * chore: react-router-dom 라이브러리 설치
    
    * feat: 라우터 설정 파일 생성
    
    * feat: notfound 페이지 추가

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { App } from './App';
+import { NotFound } from './NotFound';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
