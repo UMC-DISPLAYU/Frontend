333671c6ba72e6fa0e704f266c6eee1292e4ae9e
wolds9840 <wodls9840@gmail.com>
2026-07-02 21:56:24 +0900

    feat: 레이아웃 라우터 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { Layout } from './components/layout';
 import { App } from './App';
 import { NotFound } from './NotFound';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '*',
-    element: <NotFound />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
