c2ef08d5c85b4efbd97d212d65839e28f8e5665d
aram426 <amy0426@cau.ac.kr>
2026-07-10 14:50:39 +0900

    feat: 전시등록 페이지 화면 구현 완료

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import { Layout } from './components/layout';
 import { ArchivePage } from './pages/ArchivePage';
 import { Homepage } from './pages/Homepage';
 import { NotFound } from './pages/NotFound';
+import ExhibitionRegister from './pages/ExhibitionRegister';
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       // 새 페이지 여기에 추가
       // { path: 'about', element: <About /> },
     ],
+  },
+  {
+    path: 'exhibition-register',
+    element: <ExhibitionRegister />
   },
   {
     path: '*',
