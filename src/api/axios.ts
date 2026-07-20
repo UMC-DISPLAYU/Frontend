6a0789fedd52e0ecab4496486423039df866e052
wolds9840 <wodls9840@gmail.com>
2026-07-20 21:44:29 +0900

    feat: request interceptor 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,20 @@
 import axios from 'axios';
 
+const getAccessToken = () => localStorage.getItem('accessToken');
+
 export const axiosInstance = axios.create({
   baseURL: '',
   timeout: 10000,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  config.headers.set('Accept', 'application/json');
+
+  const accessToken = getAccessToken();
+
+  if (accessToken) {
+    config.headers.set('Authorization', `Bearer ${accessToken}`);
+  }
+
+  return config;
 });
