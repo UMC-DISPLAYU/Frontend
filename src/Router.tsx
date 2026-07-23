6ecac2834cc610e6dc78c3895a908effb4b5a69f
Awesome/Lee seung chul <chulee503@naver.com>
2026-07-23 14:51:09 +0900

    fix: 아카이브 import 오류 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import { Layout } from './components/layout';
-import { ArchivePage } from './pages/ArchivePage';
 import { AuthPage } from './pages/AuthPage';
 import { DisplayDetailPage } from './pages/DisplayDetailPage';
 import { ExhibitionRegister } from './pages/ExhibitionRegister';
@@ -20,7 +19,6 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { index: true, element: <Homepage /> },
-      { path: 'archive', element: <ArchivePage /> },
       { path: 'search', element: <SearchPage /> },
       { path: 'my', element: <MyPage /> },
       { path: 'auth', element: <AuthPage /> },
