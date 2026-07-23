fcfc365bb54a56cb90cea5de2fe1829396e70a0a
Awesome/Lee seung chul <chulee503@naver.com>
2026-07-23 17:13:49 +0900

    refactor: 라운지 content optional 제거

--- src/api/dto/lounge.dto.ts
+++ src/api/dto/lounge.dto.ts
@@ -35,7 +35,7 @@ export interface LoungePostSummaryDto {
   loungePostId: number;
   category: string;
   title: string;
-  content?: string;
+  content: string;
   postImageUrl: string | null;
   writer: LoungeWriterDto;
   createdAt: string;
