d31efe99bb6ee9d907b6a6f13587d9853239a18c
Awesome/Lee seung chul <chulee503@naver.com>
2026-08-07 05:21:21 +0900

    fix: 상단 노치 영역 채우기

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -52,15 +52,10 @@ function LoginContent({
   error?: string;
 }) {
   return (
-    <main className="relative flex h-dvh w-full flex-col overflow-hidden bg-page font-[Pretendard,sans-serif]">
-      {/* Background Image - Wall-to-Wall */}
-      <img
-        src={onboardingSplash}
-        alt=""
-        aria-hidden="true"
-        className="absolute inset-0 h-full w-full object-cover object-center"
-      />
-
+    <main
+      className="fixed inset-0 flex w-full h-full flex-col overflow-hidden bg-cover bg-top bg-no-repeat font-[Pretendard,sans-serif]"
+      style={{ backgroundImage: `url(${onboardingSplash})` }}
+    >
       {/* DU Logo & Text Overlay - Smooth Fade In */}
       <div
         className={cn(
