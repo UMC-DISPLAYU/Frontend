50e79a43cf5974e94f296b67f7439cca2755a68e
hyunmin1756 <wowangela1756@gmail.com>
2026-08-07 00:41:58 +0900

    fix: 프로필 아이콘 import 경로 수정

--- src/components/common/comment/CommentItem.tsx
+++ src/components/common/comment/CommentItem.tsx
@@ -1,6 +1,6 @@
 import { Heart } from 'lucide-react';
 
-import defaultProfileIcon from '@/assets/DefaultProfileIcon.svg';
+import defaultProfileIcon from '@/assets/common/DefaultProfileIcon.svg';
 import { cn } from '@/utils/cn';
 
 import type { CommentData } from './types';
