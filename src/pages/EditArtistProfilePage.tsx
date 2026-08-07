92cba6f9e850d5aa69106f7d4d60fa48dfd5de18
정아람 <amy0426@cau.ac.kr>
2026-08-08 03:02:55 +0900

    chore: useEffect 내 setState 문제 해결

--- src/pages/EditArtistProfilePage.tsx
+++ src/pages/EditArtistProfilePage.tsx
@@ -74,13 +74,13 @@ export function EditArtistProfilePage() {
 
   // 작가 프로필이 없으면 (404 에러만) 작가 인증 모달 표시
   useEffect(() => {
-    if (isError) {
+    if (isError && !showVerificationModal) {
       const is404 = isAxiosError(error) && error.response?.status === 404;
       if (is404) {
         setShowVerificationModal(true);
       }
     }
-  }, [isError, error]);
+  }, [isError, error, showVerificationModal]);
 
   if (showVerificationModal) {
     return (
