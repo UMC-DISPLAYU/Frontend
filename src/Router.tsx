fa5357302da0497e489a00cd979bfb66a7238302
Awesome/Lee seung chul <chulee503@naver.com>
2026-08-13 21:52:53 +0900

    fix: 내 전시 관리 보호 라우터 문제 해결

--- src/Router.tsx
+++ src/Router.tsx
@@ -165,11 +165,7 @@ export const router = createBrowserRouter([
           // 내 전시 관리 목록
           {
             path: 'my/exhibitions',
-            element: (
-              <ArtistPermissionGuard>
-                <MyExhibitionsPage />
-              </ArtistPermissionGuard>
-            ),
+            element: <MyExhibitionsPage />,
           },
 
           // 1. 전시 등록 플로우
