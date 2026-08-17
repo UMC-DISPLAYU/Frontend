63e2ae15bac7b6dd2933688081ade2abcc81b786
정아람 <amy0426@cau.ac.kr>
2026-08-17 19:42:52 +0900

    chore: 타입체크 수정

--- src/components/guards/RoutePermissionGuards.tsx
+++ src/components/guards/RoutePermissionGuards.tsx
@@ -164,6 +164,7 @@ export function DisplayCreatePermissionGuard({
 }: GuardChildrenProps & { fallback?: string }) {
   const createPolicy = useDisplayCreatePolicy();
   const policy = {
+    view: () => false,
     create: createPolicy.create,
     edit: () => false,
     delete: () => false,
