4e65a0a7221aad171942cd56f0cc9b2d43402d1d
Awesome/Lee seung chul <chulee503@naver.com>
2026-08-19 20:19:25 +0900

    refactor: WebP 고화질 png 변환

--- src/components/lounge/MyActivitySection.tsx
+++ src/components/lounge/MyActivitySection.tsx
@@ -1,9 +1,9 @@
 import { ArrowUpRight } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
-import myCommentsIcon from '@/assets/lounge/my-comments.svg';
-import savedPostsIcon from '@/assets/lounge/saved-posts.svg';
-import scrapsIcon from '@/assets/lounge/scraps.svg';
+import myCommentsIcon from '@/assets/lounge/my-comments.png';
+import savedPostsIcon from '@/assets/lounge/saved-posts.png';
+import scrapsIcon from '@/assets/lounge/scraps.png';
 
 import { LoungeCard } from './LoungeCard';
 
