8c4342f2b832154ace26d1ca06dea67e94d3c64b
JUNG ARAM <amy0426@cau.ac.kr>
2026-08-21 02:21:09 +0900

    chore: 작품 삭제 모달 수정 (#355)

--- src/components/artworks-manage/index.ts
+++ src/components/artworks-manage/index.ts
@@ -2,7 +2,6 @@ export { CardPopover } from './CardPopover';
 export { ContentCard } from './ContentCard';
 export { ContentDeleteConfirmDialog } from './ContentDeleteConfirmDialog';
 export { ContentEditSheet } from './ContentEditSheet';
-export { DeleteConfirmDialog } from './DeleteConfirmDialog';
 export { ManageScreen } from './ManageScreen';
 export { OrderScreen } from './OrderScreen';
 export { WorkActionSheet } from './WorkActionSheet';
